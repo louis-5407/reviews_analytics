1d20cb004ec07ee49e859ca72dc0c0322de1bf91
louis liao <louis.5407@gmail.com>
2023-08-17 22:43:49 +0800

    second_commit

--- read.py
+++ read.py
@@ -11,3 +11,10 @@ for line in data:
 lenth_ave = lenth / data_len
 print(lenth_ave)
 
+good = []
+for d in data:
+	if "good" in d:
+		good.append(d)
+
+print("包含good的留言有", len(good), "筆")
+print(good[0])
