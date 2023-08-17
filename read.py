c01150cbe6f1e2f236902b41c2bbc759f2c94851
louis liao <louis.5407@gmail.com>
2023-08-17 23:12:54 +0800

    清單篩選快寫法

--- read.py
+++ read.py
@@ -10,11 +10,16 @@ for line in data:
 	lenth += len(line)
 lenth_ave = lenth / data_len
 print(lenth_ave)
-
+#篩選有good的留言
 good = []
+"""
 for d in data:
 	if "good" in d:
 		good.append(d)
 
 print("包含good的留言有", len(good), "筆")
 print(good[0])
+"""
+#快寫法
+good = [d for d in data if "good" in d ]
+print("包含good的留言有", len(good), "筆")
