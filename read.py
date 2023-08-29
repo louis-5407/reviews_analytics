6a1a48d3b763a92656eaf501e88fe93570d585ee
louis liao <louis.5407@gmail.com>
2023-08-29 15:31:28 +0800

    add word count funtion

--- read.py
+++ read.py
@@ -6,6 +6,32 @@ with open("reviews.txt","r") as f:
 
 print(data[0])
 
+data_len = len(data)
+
+lenth = 0
+for line in data:
+	lenth += len(line)
+lenth_ave = lenth / data_len
+print(lenth_ave)
+#篩選有good的留言
+good = []
+
+for d in data:
+	if "good" in d:
+		good.append(d)
+
+print("包含good的留言有", len(good), "筆")
+print(good[0])
+
+#快寫法
+good = [d for d in data if "good" in d ]
+print("包含good的留言有", len(good), "筆")
+
+bad = ["bad" in d for d in data]
+print(bad)
+
+
+#文字計數
 wc ={} #word count
 for d in data:
 	words = d.split(" ")
@@ -30,31 +56,6 @@ while True:
 	else:
 		print("沒出現過")
 
-
-"""data_len = len(data)
-
-lenth = 0
-for line in data:
-	lenth += len(line)
-lenth_ave = lenth / data_len
-print(lenth_ave)
-#篩選有good的留言
-good = []
-
-for d in data:
-	if "good" in d:
-		good.append(d)
-
-print("包含good的留言有", len(good), "筆")
-print(good[0])
-
-#快寫法
-good = [d for d in data if "good" in d ]
-print("包含good的留言有", len(good), "筆")
-
-bad = ["bad" in d for d in data]
-print(bad)"""
-
-
+print("感謝你使用此功能")
 
 
